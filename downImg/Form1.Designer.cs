e4d28f50f54933c2e98bafe954d3425c60bc5e4c
Nguyễn Việt <viethhft1988@gmail.com>
2024-09-27 16:05:22 +0700

    Add project files.

---
+++ downImg/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace downImg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            textBox1 = new TextBox();
+            numericUpDown2 = new NumericUpDown();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(35, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(139, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Đường dẫn:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(128, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Từ:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(599, 19);
+            button1.Name = "button1";
+            button1.Size = new Size(140, 129);
+            button1.TabIndex = 2;
+            button1.Text = "DownLoad";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(180, 105);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(87, 39);
+            numericUpDown1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(180, 16);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(386, 39);
+            textBox1.TabIndex = 4;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(365, 105);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(87, 39);
+            numericUpDown2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(303, 107);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 32);
+            label3.TabIndex = 5;
+            label3.Text = "Đến:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(777, 185);
+            Controls.Add(numericUpDown2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown2;
+        private Label label3;
+    }
+}
---
+++ downImg/Form1.cs
@@ -0,0 +1,120 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace downImg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        label3.Text = "Đang tải.......";
+        //        SaveFileDialog save = new SaveFileDialog();
+        //        save.Filter = "All Files|*.*";
+        //        if (save.ShowDialog() == DialogResult.OK)
+        //        {
+        //            // Thiết lập ChromeDriverService và ChromeOptions
+        //            var service = ChromeDriverService.CreateDefaultService();
+        //            service.HideCommandPromptWindow = true;
+
+        //            ChromeOptions options = new ChromeOptions();
+        //            options.AddArgument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36");
+        //            options.AddArgument("--disable-infobars");
+        //            options.AddArgument("--disable-gpu");
+        //            options.AddArgument("--no-sandbox");
+        //            options.AddArgument("--disable-dev-shm-usage");
+        //            options.AddArgument("--disable-blink-features");
+        //            options.AddArgument("--disable-blink-features=AutomationControlled");
+        //            options.AddArgument("--window-size=1920,1080");
+
+
+        //            TimeSpan timeout = TimeSpan.FromSeconds(120); // Tăng thời gian chờ
+        //            using (ChromeDriver driver = new ChromeDriver(service, options, timeout))
+        //            {
+        //                int a = 0;
+        //                string basePath = save.FileName;
+
+        //                // Tạo thư mục lưu ảnh nếu chưa tồn tại
+        //                if (!Directory.Exists(basePath))
+        //                {
+        //                    Directory.CreateDirectory(basePath);
+        //                }
+
+        //                // Vòng lặp để tải ảnh từ các chương theo URL
+        //                for (int i = int.Parse(numericUpDown1.Value.ToString()); i <= int.Parse(numericUpDown2.Value.ToString()); i++)
+        //                {
+
+        //                    driver.Navigate().GoToUrl(textBox1.Text + $"/chuong-{i}");
+
+        //                    var imgElements = driver.FindElements(By.TagName("img"));
+
+        //                    string chapterPath = $@"{basePath}\chapter {a + 1}";
+
+        //                    // Tạo thư mục chapter nếu chưa tồn tại
+        //                    if (!Directory.Exists(chapterPath))
+        //                    {
+        //                        Directory.CreateDirectory(chapterPath);
+        //                    }
+
+        //                    // Tải từng ảnh trong chapter
+        //                    for (int k = 0; k < imgElements.Count; k++)
+        //                    {
+        //                        var imgPageChapter = imgElements[k];
+        //                        var imgUrl = imgPageChapter.GetAttribute("src");
+        //                        var imgName = imgPageChapter.GetAttribute("alt") ?? $"image{k + 1}";
+
+        //                        // Tải ảnh với kiểm tra lỗi
+        //                        try
+        //                        {
+        //                            using (WebClient client = new WebClient())
+        //                            {
+        //                                client.DownloadFile(new Uri(imgUrl), $@"{chapterPath}\{imgName}.png");
+        //                            }
+        //                        }
+        //                        catch (Exception imgEx)
+        //                        {
+        //                            MessageBox.Show($"Lỗi khi tải ảnh {imgName}: {imgEx.Message}");
+        //                        }
+        //                    }
+        //                    a++;
+        //                }
+        //                driver.Quit();
+        //            }
+        //            label3.Text = "Tải hoàn tất!";
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Lỗi: " + ex.Message, "Thông báo lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+
+        //    MessageBox.Show("Tải hoàn tất!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        //}
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ChromeDriver driver = new ChromeDriver();
+                for (int i = int.Parse(numericUpDown1.Value.ToString()); i <= int.Parse(numericUpDown1.Value.ToString()); i++)
+                {
+                    driver.Navigate().GoToUrl(textBox1.Text);
+                    var find = driver.FindElements(By.ClassName(""));
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
